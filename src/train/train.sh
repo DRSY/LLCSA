2318fc70d27913f9f0de8c505716e368060a2fc4
roy <rsy0702@163.com>
2021-02-09 13:26:56 +0800

    x

--- src/train/train.sh
+++ src/train/train.sh
@@ -12,11 +12,12 @@ python train.py \
   --train_batch_size 8 \
   --dev_batch_size 8 \
   --lr 3e-5 \
-  --device cuda:0 \
+  --device cuda:1 \
   --accumulate_grad_batches 2 \
   --logging_steps 100 \
   --eval_steps 1000 \
   --write_interval 50 \
   --replay_interval 400 \
   --memory \
+  --meta_replay \
   "$@"
